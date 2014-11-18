4a0a88575dab79c8c234e453204323e2ae40676e
unknown <k@laptop.com>
2014-11-18 10:50:13 +0100

    Base implementation

---
+++ src/workdir/ArrayInnlevering.java
@@ -0,0 +1,50 @@
+// Kristoffer Stranden
+
+package workdir;
+
+import java.util.Scanner;
+
+public class ArrayInnlevering
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Oppgi arrayens lengde: ");
+        int len = in.nextInt();
+
+        fyll(len);
+    }
+
+    public static int[] fyll(int len)
+    {
+        /*
+        Oprett et array 'a' med lengde 'val'.
+        For vær index i 'arr': tildel et tilfeldig nummer mellom 0 til 'val'.
+        */
+        int[] a = {};
+        return a;
+    }
+
+    public void print(int[] a)
+    {
+        /*
+        For vær index i 'a', print innholdet.
+        System.out.printf
+         */
+    }
+
+    public void sjekk(int i)
+    {
+        // For loop som validerer hvor mange indekser som har verdien 'i'.
+        // For loop som validerer hvor mange indekser som har verdien 'i'.
+    }
+
+    public int felles(int[] arrayA, int[] arrayB)
+    {
+
+        return -1;
+    }
+}
+
+
